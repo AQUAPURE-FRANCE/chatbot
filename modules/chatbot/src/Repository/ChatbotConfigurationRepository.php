c714ed77d2abc4eb2576b60a9f69f0b83e911e44
dante18 <lablandinerie2@gmail.com>
2020-08-27 09:42:00 +0200

    Add ChatbotConfiguration repository

---
+++ modules/chatbot/src/Repository/ChatbotConfigurationRepository.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace ChatBot\Repository;
+
+use Doctrine\DBAL\Connection;
+
+class ChatbotConfigurationRepository
+{
+    /**
+     * @var Connection the Database connection
+     */
+    private $connection;
+
+    /**
+     * @var string the Database prefix
+     */
+    private $databasePrefix;
+
+    /**
+     * SymfonyModuleRepository constructor.
+     * @param Connection $connection
+     * @param $databasePrefix
+     */
+    public function __construct(Connection $connection, $databasePrefix)
+    {
+        $this->connection = $connection;
+        $this->databasePrefix = $databasePrefix;
+    }
+}
